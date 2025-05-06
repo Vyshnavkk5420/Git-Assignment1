ac87d948a247b9e448c3dd74c92dafc5c9ec78fc
VYSHNAV K K <163223307+Vyshnavkk5420@users.noreply.github.com>
2025-05-06 16:43:03 +0530

    Update new2.c for checking fetch

--- new2.c
+++ new2.c
@@ -5,5 +5,6 @@ int main()
         printf("This is file2\n");
 	printf("This is new change in file2\n");
 	printf("Unstaged stashing\n");
+	printf("Iam doing a change from remote repo\n");
         return 0;
 }
